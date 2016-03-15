0823439378e3451120743c9390e009ad868d3c91
Rafael Lott <rafael.lott2@gmail.com>
2016-03-15 00:32:23 -0300

    Configurate root urls and settings file

--- game/views.py
+++ game/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'game/index.html', {})
+
+
+def check_word(request):
+    if request.method == 'POST':
+        return JsonResponse({'foo': 'bar'})
+    else:
+        return HttpResponse(status=405)
